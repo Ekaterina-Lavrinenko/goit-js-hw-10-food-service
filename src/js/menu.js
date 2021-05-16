a9ee403860932dfe595434a2990f15aca4b28fd0
Ekaterina-Lavrinenko <lavrinenko.ey@gmail.com>
2021-05-16 19:28:51 +0100

    Добавляет карточки меню и слушателя событий на switch

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,10 +4,32 @@ import menu from './menu.json';
 import '../styles.css';
 
 const menuContainer = document.querySelector('.js-menu');
+const themeSwitch = document.querySelector('.theme-switch__toggle');
 const cardsMarkup = createMenuCardsMarkup(menu);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
 menuContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
+themeSwitch.addEventListener('click', onThemeSwitchClick);
+
 function createMenuCardsMarkup(menu) {
-    return menuCardTpl(menu);
+    return menuCardsTpl(menu);
+}
+
+function onThemeSwitchClick(e) {
+    const isThemeSwatchEl = e.target.classList.contains('theme-switch__toggle');
+    if (!isThemeSwatchEl) {
+        return;
+    }
+
+    // const swatchEl = e.target;
+    // const parentTheme = swatchEl.closest('.body');
+    // parentTheme.classList.add('dark-theme');
+    // document.body.style.background = swatchEl
+
+    console.log(e.target);
 }
